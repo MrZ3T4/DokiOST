da81fe0d71a66fd025cc8372fc88ff5b6506b9bc
MrZ3T4 <zoekcoc27@gmail.com>
2021-04-28 00:19:29 -0500

    get data & downloads OK

--- app/src/main/java/dev/mrz3t4/dokiost/GetSoundtracks.java
+++ app/src/main/java/dev/mrz3t4/dokiost/GetSoundtracks.java
@@ -9,10 +9,18 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GetSoundtracks {
 
     private Context context;
+    private ArrayList<Song> songArrayList = new ArrayList<>();
+    private ArrayList<String> titles = new ArrayList<>();
+    private ArrayList<String> artist = new ArrayList<>();
+    private ArrayList<String> urlist = new ArrayList<>();
+    private ArrayList<String> urls = new ArrayList<>();
 
     public GetSoundtracks(Context context) {
         this.context = context;
@@ -28,31 +36,124 @@ public class GetSoundtracks {
 
                 Document document = Jsoup.parse(html);
 
-                System.out.println(doc);
+                //System.out.println(doc);
 
-                Elements elements = doc.select("section[class=anx-section]");
+                Elements elements;
 
-                for (Element e: elements){
 
-                    String title = e.select("span[class=cd-song]").text();
-                    String titlee = e.select("img").attr("alt");
+                if (html.contains("div class=\"ost\"")) {
+                    elements = doc.select("div[class=ost]");
 
-                    String type = e.select("div[class=anx-top]").text();
+                    // <<---- Link ---->>
 
-                    if (type.contains("Opening")){
+                    String textUrl = elements.toString();
 
-                        System.out.println("Tipo: " + type);
-                        System.out.println("TITULO: " + title);
-                    } else if (type.contains("Ending")){
+                    String regex = "\\(?\\b(https?://|www[.]|ftp://)[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]";
 
-                        System.out.println("Type: " + type);
-                        System.out.println("TITULO: " + title);
+                    Pattern p = Pattern.compile(regex);
+                    Matcher m = p.matcher(textUrl);
+
+                    while(m.find()) {
+                        String urlStr = m.group();
+                        System.out.println("Url: " + urlStr);
+                        urlist.add(urlStr);
 
                     }
 
+                    for (int pos = 1; pos < urlist.size(); pos++ ){
+                        String url = urlist.get(pos);
+                        if (!urlist.get(pos-1).isEmpty()){
+                            if (url.equalsIgnoreCase(urlist.get(pos-1))){
+                                System.out.println(url);
+                                urls.add(url);
+                            }
+                        }
+                    }
+
+                    for (Element e: elements){
+
+                        String text = e.select("span").text();
+                        String textSong;
+                        String textArtist;
+
+                        if (link.contains("bleach")){
+                            textArtist = text.replace("Daidai\"", "\"Daidai\"").replaceAll("\"([^\"]*)\"", "");
+                            textSong = text.replace("Daidai\"", "\"Daidai\"");
+                            System.out.println("xd ---> " +textArtist);
+                        } else {
+                            textArtist = text.replaceAll("\"([^\"]*)\"", "");
+                            textSong = text;
+                        }
+
+                        // <<---- Artist ---->>
+
+                        ArrayList<String> myList = new ArrayList<>(Arrays.asList(textArtist.split("by" )));
+
+                        for (int i = 0; myList.size()>i; i++){
+                            String filter = myList.get(i).replaceFirst(" ", "");
+                            if (!filter.equalsIgnoreCase("by") && !filter.isEmpty()) {
+                                System.out.println("Artist: " + filter);
+                                artist.add(filter);
+                            }
+
+                        }
+
+                        // <<---- Title ---->>
+
+                        Pattern pa = Pattern.compile("\"([^\"]*)\"");
+                        Matcher ma = pa.matcher(textSong);
+
+                        while (ma.find()) {
+                            System.out.println("Title: " + ma.group(1));
+                            titles.add(ma.group(1));
+                        }
+
+                        // <<---- Create Final ArrayList ---->>
+
+                        for (int i  = 0; i < titles.size(); i++){
+                            Song song = new Song();
+                            song.setTitle(titles.get(i));
+                            song.setArtist(artist.get(i));
+                            song.setUrl(urls.get(i));
+                            songArrayList.add(song);
+                        }
+                        for (int i  = 0; songArrayList.size() > i; i++){
+                        System.out.println("Song --> " + songArrayList.get(i).getTitle() + " by " + songArrayList.get(i).getArtist() + " --> " + songArrayList.get(i).getUrl());
+                        }
 
 
+
+                    }
+
+                System.out.println("XD OST");
+                } else {
+                    elements = doc.select("section[class=anx-section]");
+                    System.out.println("XD NOST");
+                    String path = "div[class=anx-song flex wrap]";
+
+                    if (elements.toString().contains("anx-song anx-audio flex wrap"))  path = "div[class=anx-song anx-audio flex wrap]";
+                    Elements elements1 = doc.select(path);
+
+                    for (Element e: elements){
+
+                        String type = e.select("div[class=anx-top]").text();
+
+                        if (type.equalsIgnoreCase("Opening")){
+                            for (Element test : elements1) {
+                                String title = test.select("span[class=cd-song]").text();
+                                String artist = test.select("span[class=cd-artist]").text();
+                                String download = test.select("a[class=color anx-link]").attr("href");
+
+                                if (!title.isEmpty()) System.out.println("TITULO: " + title);
+                                if (!artist.isEmpty()) System.out.println("Artista: " + artist);
+                                if (!download.isEmpty()) System.out.println("Link: " + download);
+                            }
+
+                        }
+
+                    }
                 }
+
 
 
             } catch (Exception e){
